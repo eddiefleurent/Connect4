650fd03e4a8d39efaa35605d709224ab724b9dce
Eddie Fleurent <eduardofleurent94@gmail.com>
2022-09-14 05:38:03 -0400

    implement getCol to get player input, and helper funcs

--- UserInterface.hs
+++ UserInterface.hs
@@ -1,6 +1,7 @@
 module UserInterface where
 
 import Board (Board, Player (..), cols)
+import Data.Char (digitToInt, intToDigit, isDigit)
 
 {-
 > showBoard testBoard
@@ -13,7 +14,6 @@ import Board (Board, Player (..), cols)
 -------
 0123456
 -}
-
 showBoard :: Board -> IO ()
 showBoard b =
   putStrLn (unlines (map showRow b ++ [line] ++ [nums]))
@@ -36,3 +36,41 @@ showPlayer :: Player -> Char
 showPlayer O = 'O'
 showPlayer B = '.'
 showPlayer X = 'X'
+
+-- >>> turn B
+-- B player cannot take a turn
+-- >>> turn X
+-- O
+-- >>> turn O
+-- X
+turn :: Player -> Player
+turn X = O
+turn O = X
+turn _ = error "B player cannot take a turn"
+
+movePrompt :: Player -> String
+movePrompt p = concat ["Show me what you got, player ", show p, "!\n Enter a column number from 0 to ", show cols, "to drop your stone.\nDon't think too much, you'll lose anyways."]
+
+-- >>> validateInt "1"
+-- True
+-- >>> validateInt "9"
+-- False
+-- >>> validateInt "111"
+-- False
+validateInt :: String -> Bool
+validateInt i
+  | length i == 1 && isDigit ih && ih `elem` ['0' .. colsC] = True
+  | otherwise = False
+  where
+    ih = head i --safe to call head as emptiness is validated
+    colsC = intToDigit cols
+
+getCol :: Player -> IO Int
+getCol p = do
+  putStrLn $ movePrompt p
+  inp <- getLine
+  if validateInt inp
+    then pure $ digitToInt $ head inp -- safe head call
+    else do
+      putStrLn "You expect to beat me, but you can't even follow simple instructions? Try again..."
+      getCol p
